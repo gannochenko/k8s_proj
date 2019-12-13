6c443c3c0313e34af8bb2786f25923dacbf71165
Sergei <gannochenko.sv@gmail.com>
2019-12-13 21:03:54 +0100

    woops

--- script/plan.sh
+++ script/plan.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 ENV="${1:-prod}"
 
-cd ${DIR}/../infra/terraform/production;
+cd ${DIR}/../infra/terraform/;
 
 terraform plan -var="env=${ENV}" -state=./${ENV}/terraform.fstate;
