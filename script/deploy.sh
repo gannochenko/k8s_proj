918da9dade361eaad7597529cde27d3a77d188ce
Sergei <gannochenko.sv@gmail.com>
2019-12-13 21:31:47 +0100

    deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,11 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 
-cd ${DIR}/../infra/terraform;
+cd ${DIR}/../infra/terraform/;
 
 if [[ ! -d ./.terraform ]]; then
     terraform init
     terraform get
 fi
 
-terraform apply -auto-approve;
+terraform apply -auto-approve -var="env=${ENV}" -state=./state/${ENV}/terraform.tfstate -state-out=./state/${ENV}/terraform.tfstate;
