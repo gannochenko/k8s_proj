8d6d2e633b43ab9c345812ecd83f51ec65015990
Sergei <gannochenko.sv@gmail.com>
2019-12-13 20:31:34 +0100

    another fix

--- infra/terraform/production/locals.tf
+++ infra/terraform/production/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  namespace = "k8s-proj-${var.env}}"
+  namespace = "k8s-proj-${var.env}"
   urlpx = var.env == "prod" ? "" : "-${var.env}"
-  baseurl = "balticlegacy${local.urlpx}}.ru"
+  baseurl = "balticlegacy${local.urlpx}.ru"
 }
